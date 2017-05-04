a581a232e534ba40ba8a43c36b22cc0f998d44f5
zdravkop <zdravkop@rila.bg>
2017-05-04 18:28:17 +0300

    Create main screen: progress bars.

--- app/src/main/java/com/kirilv/android/splitme/MainActivity.java
+++ app/src/main/java/com/kirilv/android/splitme/MainActivity.java
@@ -1,44 +1,46 @@
 package com.kirilv.android.splitme;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.util.SparseArray;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.View.OnTouchListener;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.SeekBar;
-import android.widget.TextView;
 
-import com.kirilv.android.splitme.model.User;
+import android.view.View;
+
+import android.widget.Button;
+
+import android.widget.ProgressBar;
+
+
 
 public class MainActivity extends Activity {
 
+	private ProgressBar overallBar;
+
+	private ProgressBar dailyBar;
+
+	private Button addIncome;
+
+	private Button addExpense;
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+
+	public void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_main);
-		setTitle(getResources().getString(R.string.lbl_title_members).toString());
+
+		overallBar = (ProgressBar) findViewById(R.id.overallBar);
+		overallBar.setVisibility(View.VISIBLE);
+		overallBar.setMax(100);
+		overallBar.setProgress(50);
+
+		dailyBar = (ProgressBar) findViewById(R.id.dailyBar);
+
+		addIncome = (Button) findViewById(R.id.incomeBtn);
+
+		addExpense = (Button) findViewById(R.id.expenseBtn);
 
 	}
+
 }
