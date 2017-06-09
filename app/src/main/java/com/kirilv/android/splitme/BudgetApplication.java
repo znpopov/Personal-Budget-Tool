2bdb7a14a97572411f4d519a743b628944622052
slavunderkind <smcankova@gmail.com>
2017-06-09 18:18:00 +0300

    Implement progress bar at category list view

--- app/src/main/java/com/kirilv/android/splitme/BudgetApplication.java
+++ app/src/main/java/com/kirilv/android/splitme/BudgetApplication.java
@@ -10,20 +10,16 @@ import com.kirilv.android.splitme.model.TransactionDBHelper;
 
 import java.util.HashMap;
 
-public class BudgetApplication extends Application
-{
+public class BudgetApplication extends Application {
     private static BudgetApplication ourInstance;
 
     private HashMap<Long, Category> categories;
     private HashMap<Long, Transaction> transactions;
     private CategoryDBHelper categoryDBHelper;
     private TransactionDBHelper transactionDBHelper;
+    private HashMap<Long, Double> categoryAmounts;
 
-    public static BudgetApplication getInstance()
-    {
-//        if(ourInstance == null){
-//            ourInstance = new BudgetApplication();
-//        }
+    public static BudgetApplication getInstance() {
         return ourInstance;
     }
 
@@ -31,10 +27,14 @@ public class BudgetApplication extends Application
         return categories;
     }
 
-    public HashMap<Long, Transaction> getTransactions(){
+
+    public HashMap<Long, Transaction> getTransactions() {
         return transactions;
     }
 
+    public HashMap<Long, Double> getCategoryAmounts() {
+        return categoryAmounts;
+    }
     @Override
     public void onCreate() {
         super.onCreate();
@@ -44,16 +44,23 @@ public class BudgetApplication extends Application
 
         categories = new HashMap<>();
         transactions = new HashMap<>();
+        categoryAmounts = new HashMap<>();
 
-        for(Category category : categoryDBHelper.getAllCategories()){
+        for (Category category : categoryDBHelper.getAllCategories()) {
             categories.put(category.getId(), category);
         }
 
-        for(Transaction transaction : transactionDBHelper.getAllTransaction()){
+        for (Transaction transaction : transactionDBHelper.getAllTransaction()) {
             transactions.put(transaction.getId(), transaction);
+            long categoryId = transaction.getCategoryId();
+            if (categoryAmounts.get(categoryId) != null) {
+                double value = categoryAmounts.get(categoryId);
+                value += transaction.getAmount();
+                categoryAmounts.put(categoryId, value);
+            } else {
+                categoryAmounts.put(categoryId, transaction.getAmount());
+            }
         }
-
-        int transactionSize = transactions.size();
     }
 
     public CategoryDBHelper getCategoryDBHelper() {
--- app/src/main/java/com/kirilv/android/splitme/CategoryAdapter.java
+++ app/src/main/java/com/kirilv/android/splitme/CategoryAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,17 +15,21 @@ import com.kirilv.android.splitme.model.Category;
 import java.util.ArrayList;
 import java.util.List;
 
-public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHolder> {
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoriesViewHolder> {
 
     private ArrayList<Category> categories;
+    private static final int MAX_PROGRESSBAR_VALUE = 100;
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class CategoriesViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
         public TextView mTextView;
+        public ProgressBar progressBar;
 
-        public ViewHolder(TextView v) {
+        public CategoriesViewHolder(View v) {
             super(v);
-            mTextView = v;
+            mTextView = (TextView) v.findViewById(R.id.name);
+            progressBar = (ProgressBar) v.findViewById(R.id.progressBar);
+            progressBar.setMax(MAX_PROGRESSBAR_VALUE);
         }
     }
 
@@ -33,19 +38,21 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.ViewHo
     }
 
     @Override
-    public CategoryAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+    public CategoryAdapter.CategoriesViewHolder onCreateViewHolder(ViewGroup parent,
                                                    int viewType) {
-        // Get the data item for this position
-        // create a new view
-        TextView categoryName = (TextView) LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.category_item, parent, false);
-
-        return new ViewHolder(categoryName);
+        return new CategoriesViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.category_item, parent, false));
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, final int position) {
-        holder.mTextView.setText(categories.get(position).getName());
+    public void onBindViewHolder(CategoriesViewHolder holder, final int position) {
+        Category category = categories.get(position);
+        int categoryAmount = 0;
+        if (BudgetApplication.getInstance().getCategoryAmounts().get(category.getId()) != null){
+            categoryAmount = BudgetApplication.getInstance().getCategoryAmounts().get(category.getId()).intValue();
+        }
+        holder.mTextView.setText(category.getName());
+        holder.progressBar.setProgress(categoryAmount);
+
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
